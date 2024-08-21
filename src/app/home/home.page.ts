e43597972a89c3b6954bf208eac4b6d16f6815ad
Chanantida Krungmee <Cntdkm.kim@gmail.com>
2024-08-21 14:16:22 +0700

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private t : ToastController) {}
+
+  async presentToast(position:'bottom') {
+    const toast = await this.t.create({
+      message: 'Hello World!',
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
+  
+}
